293d6f5380d979cd9a0e77c576632756400a9cb2
bukhari-work <ahmadbukharkwork@gmail.com>
2025-03-13 13:00:20 +0800

    Added project folder for Search

--- scripts/jsonGenerator.js
+++ scripts/jsonGenerator.js
@@ -6,6 +6,7 @@ const CONTENT_DEPTH = 2;
 const JSON_FOLDER = "./.json";
 
 // Define your directories
+const PROJECT_FOLDER = "src/content/project";
 const BLOG_FOLDER = "src/content/blog";
 const AUTHORS_FOLDER = "src/content/authors";
 
@@ -57,11 +58,12 @@ try {
   }
 
   // Get data from each directory
+  const projectData = getData(PROJECT_FOLDER, 2);
   const blogData = getData(BLOG_FOLDER, 2);
   const authorsData = getData(AUTHORS_FOLDER, 2);
 
   // Merge all data into one array
-  const combinedData = [...blogData, ...authorsData];
+  const combinedData = [...projectData, ...blogData, ...authorsData];
 
   // Write combined data to posts.json
   fs.writeFileSync(`${JSON_FOLDER}/posts.json`, JSON.stringify(combinedData));
